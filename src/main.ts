ba33e71dd4832db93807c7f1d95cea197543ccc6
Baptiste Pillot <baptiste@pillot.fr>
2025-11-13 16:34:01 +0100

    Allow configurable listening host

--- src/main.ts
+++ src/main.ts
@@ -75,6 +75,7 @@ export async function run()
 		execute:     request => execute(new Request(request)),
 		favicon:     '/node_modules/@itrocks/framework/favicon.ico',
 		frontScripts,
+		host:        config.host ?? '127.0.0.1',
 		port:        3000,
 		scriptCalls: ['loadCss', 'loadScript'],
 		secret:      config.secret,
